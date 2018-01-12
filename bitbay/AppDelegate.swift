1db2d2034d4de5047fe1618af3515fbc563ea137
Bartosz Walendziuk <bwalendziuk@9bits.pl>
2018-01-12 21:08:49 +0100

    Initial Commit

---
+++ bitbay/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  bitbay
+//
+//  Created by Bartosz Walendziuk on 12.01.2018.
+//  Copyright © 2018 Bartosz Walendziuk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
